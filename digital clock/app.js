d7627ab96c6da084499fe444e94b05c9554d39e7
Tanveer Khan <tanveer@Tanveers-MacBook-Air.local>
2026-01-15 13:04:34 +0530

    digial clock padding

--- digital clock/app.js
+++ digital clock/app.js
@@ -5,7 +5,7 @@ let secs = document.getElementById("secs");
 setInterval( () => {
     let currentTime = new Date();
 
-    hrs.innerHTML = currentTime.getHours();
-    mins.innerHTML = currentTime.getMinutes();
-    secs.innerHTML = currentTime.getSeconds();
+    hrs.innerHTML = String(currentTime.getHours()).padStart(2, "0");
+    mins.innerHTML = String(currentTime.getMinutes()).padStart(2, "0");
+    secs.innerHTML = String(currentTime.getSeconds()).padStart(2, "0");
 }, 1000)
